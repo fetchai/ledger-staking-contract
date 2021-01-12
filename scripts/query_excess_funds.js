7f0d0d3d22ba6a802b89f430d31e5df580984cc4
Peter Bukva <peter.bukva@fetch.ai>
2021-01-12 00:15:55 +0000

    Script for querying all excess funds in the skaing contract (mainnet).

---
+++ scripts/query_excess_funds.js
@@ -0,0 +1,188 @@
+const Web3 = require('web3');
+const fs = require('fs')
+const { BN } = require('bn.js')
+const { Decimal } = require('decimal.js')
+
+const decimalPrecision = 50;
+const fetErc20CanonicalMultiplier = new Decimal('1e18');
+
+function dumpToJsonFile(path, obj, ...contents) {
+    fs.writeFileSync(path, JSON.stringify(obj, ...contents));
+}
+
+function canonicalFetToFet(canonicalVal) {
+    const origPrecision = Decimal.precision
+    Decimal.set({ precision: decimalPrecision })
+    try {
+        return (new Decimal(canonicalVal.toString())).div(fetErc20CanonicalMultiplier)
+    }
+    finally {
+        Decimal.set({ precision: origPrecision })
+    }
+}
+
+const dir = __dirname;
+const abi_IERC20 = require(`${dir}/IERC20.json`).abi;
+const abi_Staking = require(`${dir}/Staking.json`).abi;
+const token_contract_address = "0xaea46A60368A7bD060eec7DF8CBa43b7EF41Ad85";
+const staking_contract_address = "0x351baC612B50e87B46e4b10A282f632D41397DE2";
+//const mnemonic = fs.readFileSync(`${dir}/../.secrets_mnemonic_mainnet`).toString().trim();
+const infuraProjectId = fs.readFileSync(`${dir}/../.secrets_infura_project_id`).toString().trim();
+//const _endpoint = `https://mainnet.infura.io/v3`;
+const _endpoint = 'wss://mainnet.infura.io/ws/v3';
+let endpoint = `${_endpoint}/${infuraProjectId}`;
+console.log(`endpoint = "${endpoint}"`);
+
+//const wsProvider = new Web3.providers.WebsocketProvider(endpoint);
+//const web3 = new Web3(wsProvider);
+const web3 = new Web3(endpoint);
+
+//const token = new web3.eth.Contract(JSON.parse(abi), token_contract_address);
+const token = new web3.eth.Contract(abi_IERC20, token_contract_address);
+const staking = new web3.eth.Contract(abi_Staking, staking_contract_address);
+
+//token.events.allEvents(
+//    {
+//                filter: {to: '0x351baC612B50e87B46e4b10A282f632D41397DE2'},
+//                fromBlock: "10998076",
+//                toBlock: "10999076"
+//        },
+//    (error, event) => { if (error) {
+//           console.log("error: ", error);
+//        }
+//        console.log("event:", event);
+//   }
+//   ).on("connected", function(subscriptionId){
+//       console.log(subscriptionId);
+//   })
+//   .on('data', function(event){
+//       console.log("event{data}: ", event); // same results as the optional callback above
+//   })
+//   .on('changed', function(event){
+//       console.log("event{changed}: ", event); // same results as the optional callback above
+//   })
+//   .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+//       console.log("error: ", error, ", receip: ", receipt); // same results as the optional callback above
+//   });
+
+//token.events.Transfer({
+//   filter: {to: '0x351baC612B50e87B46e4b10A282f632D41397DE2'},
+//            fromBlock: "10998076",
+//            //toBlock: "10999100",
+//   }, (error, event) => {
+//       if (error) {
+//           console.log("error: ", error);
+//       }
+//       console.log("event:", event);
+//   }).on("connected", function(subscriptionId){
+//       console.log(subscriptionId);
+//   })
+//   .on('data', function(event){
+//       console.log("event{data}: ", event); // same results as the optional callback above
+//   })
+//   .on('changed', function(event){
+//       console.log("event{changed}: ", event); // same results as the optional callback above
+//   })
+//   .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+//       console.log("error: ", error, ", receip: ", receipt); // same results as the optional callback above
+//   });
+
+//evnts = []
+
+//token.events.Transfer({
+//   filter: {to: '0x351baC612B50e87B46e4b10A282f632D41397DE2'},
+//            fromBlock: "10998076",
+//            //toBlock: "11634233",
+//            //toBlock: "latest",
+//   }, (error, event) => {
+//       if (error) {
+//           console.log("error: ", error);
+//       }
+//       evnts.push(event);
+//       console.log("event:", event);
+//   })
+
+
+async function main () {
+    //const end_block = await web3.eth.getBlockNumber();
+    const retval = {}
+    await token.getPastEvents("Transfer", {
+        filter: {to: '0x351baC612B50e87B46e4b10A282f632D41397DE2'},
+        fromBlock: "10998076",
+        toBlock: "latest",
+    }, (error, events) => {
+        if (error) {
+            console.log("error: ", error);
+            throw error;
+        }
+        retval.erc20 = {};
+        retval.erc20.events_list = events;
+        //console.log("events:", events);
+    });
+
+    retval.staking = {};
+    retval.staking.events_list = [];
+    retval.staking.events_dict = {};
+
+    staking_event_names = ["LiquidityDeposited", "RewardsPoolTokenTopUp"];
+    for (let i = 0; i<staking_event_names.length; ++i) {
+        const evt_name = staking_event_names[i];
+        await staking.getPastEvents(evt_name, {
+            fromBlock: "11061460",
+            toBlock: "latest",
+            },
+            (error, events) => {
+                if (error) {
+                    console.log("error: ", error);
+                    throw error;
+                }
+                retval.staking.events_list = retval.staking.events_list.concat(events);
+                const evts_dict = retval.staking.events_dict;
+
+                console.log("Number of \"", evt_name, "\" events: ", events.length);
+                for (let i = 0; i < events.length; ++i) {
+                    const e = events[i];
+                    //console.log("e: ", e);
+                    if (e.transactionHash in evts_dict) {
+                        evts_dict[e.transactionHash].push(e);
+                    } else {
+                        evts_dict[e.transactionHash] = [e];
+                    }
+                }
+
+                //console.log("events:", events);
+            });
+    }
+
+    const aggregate = new BN("0");
+
+    const events = retval.erc20.events_list;
+    const staking_evts_dict = retval.staking.events_dict;
+    const exces_funds_events_list = [];
+    retval.exces_funds_events_list = exces_funds_events_list;
+
+    for (let i=0; i< events.length; ++i) {
+        const e = events[i];
+        if (! (e.transactionHash in staking_evts_dict)) {
+            exces_funds_events_list.push(e);
+            //const tx = await web3.eth.getTransactionReceipt(e.transactionHash);
+            //console.log("excess transfer tx: ", tx);
+            const transfer_amount = new BN(e.returnValues.value);
+            console.log("excess transfer[", exces_funds_events_list.length - 1, "]: ", canonicalFetToFet(transfer_amount).toString(), " [FET] = ", transfer_amount.toString(), ` [Canonical FET] | {https://etherscan.io/tx/${e.transactionHash}}`);
+            aggregate.iadd(transfer_amount);
+        }
+    }
+    const aggregateDec = canonicalFetToFet(aggregate);
+    console.log("Number of events:", exces_funds_events_list.length);
+    console.log("Aggregated value: ", aggregateDec.toString(), " [FET] = ", aggregate.toString(), " [Canonical FET]");
+    dumpToJsonFile(`${dir}/events.json`, retval, null, "  ");
+
+    const principal = new BN(await staking.methods._accruedGlobalPrincipal().call());
+    const rewards_pool_balance = new BN(await staking.methods.getRewardsPoolBalance().call());
+    const balance = new BN(await token.methods.balanceOf(staking_contract_address).call());
+    const expected_excess_funds_amount = balance.sub(principal.add(rewards_pool_balance));
+    const expected_excess_funds_amount_dec = canonicalFetToFet(expected_excess_funds_amount);
+    console.log("Expected value  : ", expected_excess_funds_amount_dec.toString(), " [FET] = ", expected_excess_funds_amount.toString(), " [Canonical FET]");
+}
+
+main();
